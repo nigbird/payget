7279b5e8ace8b7930712d47a2b003e7b6c0cfe3e
hawiti <hawitulu71@gmail.com>
2026-04-03 15:12:06 +0300

    auth error fix

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,7 @@ import { db } from "@/app/lib/db"
 import bcrypt from "bcryptjs"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Credentials({
       credentials: {
