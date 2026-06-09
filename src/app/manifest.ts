ee2c3bbe70f5ce5af26519dd02ca7d94fb072028
nigbird <birdnig00@gmail.com>
2026-06-09 14:06:13 +0300

    removed install btn

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait',
     icons: [
       {
-        src: '/niblogo.png',
+        src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/niblogo.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
